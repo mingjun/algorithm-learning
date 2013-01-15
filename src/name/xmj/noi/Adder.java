faee9e9ca753d86d4e94dcc42a14b2c243ebd3b1
Mingjun Xu <xmj0236@gmail.com>
2013-01-15 22:11:34 +0800

    complete adder

--- src/name/xmj/noi/Adder.java
+++ src/name/xmj/noi/Adder.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 /**
  * http://judge.noi.cn/problem?id=1000
  * advanced version
- * 
+ *
  * @author mingjun
  *
  */
@@ -13,6 +13,7 @@ public class Adder {
 
 	int [] x;
 	int [] y;
+	final int mode = 10;
 	public Adder(String a, String b) {
 		x = new int[a.length()];
 		y = new int[b.length()];
@@ -23,10 +24,37 @@ public class Adder {
 			y[i] = b.charAt(i) - '0';
 		}
 	}
+	int [] add() {
+		int maxLength = Math.max(x.length, y.length);
+		int [] sum = new int[maxLength + 1];
+		int i = x.length-1, j = y.length-1, k = sum.length-1;
+		int overflow = 0;
+		while(k >= 0) {
+			int x_i = i >= 0 ? x[i] : 0;
+			int y_j = j >= 0 ? y[j] : 0;
+			int sss = x_i + y_j + overflow;
+			sum[k] = sss % mode;
+			overflow = sss / mode;
+			i--;
+			j--;
+			k--;
+		}
+		return sum;
+	}
+
+	String run() {
+		return format(add());
+	}
+
+	String format(int [] number) {
+		return Arrays.toString(number);
+	}
+
 
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(Arrays.toString(x)).append(" + ").append(Arrays.toString(y));
+		sb.append(format(x)).append(" + ").append(format(y));
+		sb.append("=").append(run());
 		return sb.toString();
 	}
 }
--- test/name/xmj/noi/TestAdder.java
+++ test/name/xmj/noi/TestAdder.java
@@ -1,12 +1,43 @@
 package name.xmj.noi;
 
+import java.math.BigInteger;
+import java.util.Random;
+
 import org.junit.Test;
 
 public class TestAdder {
 	@Test
 	public void testToString() {
-		Adder adder = new Adder("123", "456");
+		Adder adder = new Adder("1234", "456");
 		System.out.println(adder);
 	}
+
+	static Random r = new Random();
+	static String x = String.valueOf(Math.abs(r.nextLong()));
+	static String y = String.valueOf(Math.abs(r.nextLong()));
+
+	static  {
+		StringBuilder sb1 = new StringBuilder(), sb2 = new StringBuilder();
+		for(int i=0;i<1000;i++) {
+			sb1.append(x);
+			sb2.append(y);
+		}
+		x = sb1.toString();
+		y = sb2.toString();
+	}
+	@Test
+	public void testAddPerform() {
+		String z = new Adder(x,y).run();
+		System.out.println(z.length());
+	}
+
+	@Test
+	public void testBigIntegerPerform() {
+		BigInteger a =  new BigInteger(x);
+		BigInteger b =  new BigInteger(y);
+		BigInteger z = a.add(b);
+		System.out.println(z.toString().length());
+	}
+
 
 }
