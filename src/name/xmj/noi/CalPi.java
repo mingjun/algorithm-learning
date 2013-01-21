585ad118e427ff475b2f3f0b2ed45fe4a0f9d1a3
Mingjun Xu <xmj0236@gmail.com>
2013-01-21 22:29:23 +0800

    complete pi (bad performance)

--- src/name/xmj/noi/CalPi.java
+++ src/name/xmj/noi/CalPi.java
@@ -13,7 +13,7 @@ public class CalPi {
 	/**
 	 * pi/2 = 1 + 1/3 + 1/3* 2/5 + 1/3* 2/5 * 3/7 + ...
 	 * area = pi * r*r;
-	 * area in ¡À0.001  => pi in ¡À 0.001/r/r;
+	 * area in +-0.001  => pi in +-0.001/r/r;
 	 */
 	public static double runSimple(double r) {
 		double tiny = Math.pow(10, -piLength(r));
@@ -36,23 +36,34 @@ public class CalPi {
 	public static int piLength(double r) {
 		int r_order = (int)Math.ceil(Math.log(r)/Math.log(10));
 		int correctOrder = 6 + r_order + r_order;
-//		double tiny = Math.pow(10, correctOrder);
 		return correctOrder;
 	}
 
-	int [] pi, temp;
+	int [] pi, iterator, temp;
+	final static int mode = 10; //TODO improve performance
 	public CalPi(double r) {
-		int len = piLength(r) + 1;
-		System.out.println(len);
+		this(piLength(r) + 1);
+	}
+
+	public CalPi(int len) {
 		pi = new int[len];
+		iterator = new int[len];
 		temp = new int[len];
 	}
 
 	public void run() {
+		final int len = pi.length;
 		pi[0]=2;
-//		int i=1,j=3;
-		smallDiv(2,3, this.temp);
-		//TODO MUL  ADD
+		int i=1,j=3;
+		smallDiv(2,3, iterator);
+		while(i<1000000) {
+			addIn(pi, iterator, len);
+			i++; j+=2;
+			iterator = multipleIn(iterator, smallDiv(i, j, temp), len-1);
+			if(isTiny(iterator)) break;
+			
+		}
+		System.out.println(i+" pi="+format(pi));
 	}
 
 	public int [] smallDiv(int x, int y, int [] temp) {
@@ -63,11 +74,62 @@ public class CalPi {
 			temp[i] = quotient;
 			a = remain * 10;
 		}
-		System.out.println(format(temp));
 		return this.temp;
 	}
 
+	public int [] addIn(int [] x, int [] y, int len) {
+		int overflow = 0;
+		for(int i=len-1;i>=0; i--) {
+			int sum = x[i] + y[i] + overflow;
+			x[i] = sum % mode;
+			overflow = sum / mode;
+		}
+		return x;
+	}
 
+	/**
+	 * 
+	 * assumption: x [0,1,2,3,...] means 0.123...
+	 * assumption: y [0,3,4,5,...] means 0.345...
+	 * the length of x y is (len+1);
+	 * 
+	 * @param x
+	 * @param y
+	 * @param len
+	 * @return
+	 */
+	public int [] multipleIn(int [] x, int [] y, int len) {
+		int [] product = new int [len*2];
+		
+		for(int i=len;i>0;i--) {
+			for(int j=len;j>0;j--) {
+				int ii = len - i;
+				int jj = len - j;
+				int kk =  ii + jj;
+				int k =  product.length -1 - kk;
+				int p = x[i] * y[j] + product[k];
+				product[k] = p % mode;
+				product[k-1] += p / mode;
+			}
+		}
+
+		for(int i=1; i<= len;i++) {
+			x[i] = product[i-1];
+		}
+		//round
+		if(product[len] >= 5) {
+			x[len] ++;
+		}
+		return x;
+	}
+
+	boolean isTiny(int [] x) {
+		boolean zero = true;
+		for(int i=0;i<x.length;i++) {
+			zero = zero && x[i] == 0;
+		}
+		return zero;
+	}
 
 	String format(int [] number) {
 		return Arrays.toString(number);
--- test/name/xmj/noi/TestPi.java
+++ test/name/xmj/noi/TestPi.java
@@ -14,5 +14,44 @@ public class TestPi {
 		CalPi c = new CalPi(1.0);
 		c.smallDiv(5, 3, c.temp);
 	}
+	
+	@Test
+	public void testAddIn() {
+		CalPi c = new CalPi(100.0);
+		c.smallDiv(1, 7, c.temp);
+		c.smallDiv(2, 3, c.pi);
+		int len = c.pi.length;
+		System.out.println("temp = " + c.format(c.temp));
+		System.out.println("pi =" + c.format(c.pi));
+		System.out.println("addin =" + c.format(c.addIn(c.pi, c.temp, len)));
+	}
+	
+	@Test
+	public void testMultipleIn() {
+		CalPi c = new CalPi(1.0);
+		int [] x = new int[] {0,3,3,3};
+		int [] y = new int[] {0,3,3,3};
+		int len = x.length -1 ;
+		System.out.println("x = " + c.format(x));
+		System.out.println("y =" + c.format(y));
+		System.out.println("MultipleIn =" + c.format(c.multipleIn(x, y, len)));
+	}
+	
+	@Test
+	public void testMultipleInReal() {
+		CalPi c = new CalPi(100.0);
+		c.smallDiv(1, 7, c.temp);
+		c.smallDiv(1, 9, c.pi);
+		int len = c.pi.length -1 ;
+		System.out.println("x = " + c.format(c.pi));
+		System.out.println("y =" + c.format(c.temp));
+		System.out.println("MultipleIn =" + c.format(c.multipleIn(c.pi, c.temp, len)));
+	}
+	
+	@Test
+	public void testPi() {
+		CalPi c = new CalPi(400);
+		c.run();
+	}
 
 }
